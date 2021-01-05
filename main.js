d4402449e86bb303665e163d90bb4f0f637e76ec
AJthebeast <75455189+AJthebeast@users.noreply.github.com>
2021-01-04 17:08:53 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+CanvasGradient.addEventListner("mousedown", my_mousedowm);
+function my_mousedown(e)
+{
+color=document.getElementById(color).Value;
+width_of_line=document.getElementById("width_of_line").value;
+radius=document.getElementById("radius").value;
+
+}
